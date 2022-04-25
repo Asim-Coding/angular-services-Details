0ee8e44963aae54e59c93d0d4831a0670a2530d3
Asim-Coding <muhammadasim19955@gmail.com>
2022-04-25 14:55:28 +0500

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component, VERSION } from '@angular/core';
+import {MessageService} from './message.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  providers:[MessageService]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  name = 'Angular d ' + VERSION.major;
+  msg:string;
+  constructor(private _messageservice:MessageService){
+
+  }
+  ngOnInit(){
+    this.msg=this._messageservice.getmessage();
+  }
 }
---
+++ src/app/message.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MessageService {
+
+  constructor() {
+ 
+   }
+   getmessage(){
+    return 'welcome digisoft solution'
+   }
+
+}
